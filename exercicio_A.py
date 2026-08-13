77249d2feb0b574c70ddab665e57899dca3b4b17
laviniahr <laviniahenriqueramos18062007@gmail.com>
2026-08-13 10:36:57 -0300

    Add files via upload

---
+++ exercicio_A.py
@@ -0,0 +1,4 @@
+# EXERCÍCIO A: Conversão Celsius para Fahrenheit
+celsius = float(input("Digite a temperatura em Celsius: "))
+fahrenheit = (9 * celsius + 160) / 5
+print(f"A temperatura em Fahrenheit é: {fahrenheit}")
