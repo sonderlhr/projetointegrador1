2ea18532aa2dcc556000c0a5fd17d8a077876d26
laviniahr <laviniahenriqueramos18062007@gmail.com>
2026-08-13 13:33:35 -0300

    Add files via upload

---
+++ exercicio_B.py
@@ -0,0 +1,8 @@
+# Programa para calcular o volume de uma lata de óleo
+
+raio = float(input("Digite o raio da lata: "))
+altura = float(input("Digite a altura da lata: "))
+
+volume = 3.14159 * (raio ** 2) * altura
+
+print(f"O volume da lata é: {volume}")
