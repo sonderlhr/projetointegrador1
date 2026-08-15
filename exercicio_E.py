f1acf0771f3507e72548796087c0714ef6f61ca0
Lavínia Henrique <laviniahenriqueramos18062007@gmail.com>
2026-08-15 14:43:15 -0300

    Delete exercicio_E.py

--- exercicio_E.py
+++
@@ -1,7 +0,0 @@
-comprimento = float(input("Digite o comprimento da caixa: "))
-largura = float(input("Digite a largura da caixa: "))
-altura = float(input("Digite a altura da caixa: "))
-
-volume = comprimento * largura * altura
-
-print(f"O volume da caixa retangular é: {volume}")
