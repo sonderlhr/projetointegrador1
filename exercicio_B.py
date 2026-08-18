18610d6966243902f16aab53ef64bf02fb028edf
Lavínia Henrique <laviniahenriqueramos18062007@gmail.com>
2026-08-18 11:03:50 -0300

    Add files via upload

---
+++ exercicio_B.py
@@ -0,0 +1,6 @@
+raio = float(input("Digite o raio da lata: "))
+altura = float(input("Digite a altura da lata: "))
+
+volume = 3.14159 * (raio ** 2) * altura
+
+print(f"O volume da lata é: {volume}")
---
+++ exercicio_E.py
@@ -0,0 +1,7 @@
+comprimento = float(input("Digite o comprimento da caixa: "))
+largura = float(input("Digite a largura da caixa: "))
+altura = float(input("Digite a altura da caixa: "))
+
+volume = comprimento * largura * altura
+
+print(f"O volume da caixa retangular é: {volume}")
