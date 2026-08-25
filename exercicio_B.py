a4b44fa2cb0da3b844eb13246b5810653c68c9e2
Lavínia Henrique <laviniahenriqueramos18062007@gmail.com>
2026-08-25 11:52:07 -0300

    Delete exercicio_B.py

--- exercicio_B.py
+++
@@ -1,6 +0,0 @@
-raio = float(input("Digite o raio da lata: "))
-altura = float(input("Digite a altura da lata: "))
-
-volume = 3.14159 * (raio ** 2) * altura
-
-print(f"O volume da lata é: {volume}")
